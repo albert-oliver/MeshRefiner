6bbb461e35130628e3bac30e59482a2938fee666
Pawel Maczuga <pmaczuga97@gmail.com>
2021-12-29 14:25:44 +0100

    Changed error calculation in adapt_fun to absolute and added logging

--- src/adaptation/adapt_fun.jl
+++ src/adaptation/adapt_fun.jl
@@ -9,9 +9,9 @@ to function `fun`"
 function error_rel(g::HyperGraph, fun, i)
     v1, v2, v3 = interiors_vertices(g, i)
     center = mean([coords2D(g, v1), coords2D(g, v2), coords2D(g, v3)])
-    uh = mean([get_value(g, v1), get_value(g, v2), get_value(g, v3)])
+    uh = mean(fun.(map(x -> coords2D(g, x), [v1, v2, v3])))
 
-    return (fun(center) - uh)^2 / fun(center)^2
+    return (fun(center) - uh)^2
 end
 
 "Match vertices of graph `g` to values of function `fun`. Value is set in
@@ -28,25 +28,33 @@ end
 Adapt graphs `g` to approximate function `fun`. The more iterations `iters` the
 better approximation.
 """
-function adapt_fun!(g, fun, iters)
-    max_error = 0
-    errors = Dict()
-    match_to_fun!(g, fun)
+function adapt_fun!(g, fun, iters; log=true)
     for i = 1:iters
+        if log
+            print("Iteration $i")
+        end
+
+        max_error = 0
+        errors = Dict()
         for e in interiors(g)
             error = error_rel(g, fun, e)
             errors[e] = error
             max_error = max(max_error, error)
         end
 
+        count = 0
         for e in interiors(g)
             error = errors[e]
             if (error) > 0.33 * max_error
                 set_refine!(g, e)
+                count += 1
             end
         end
 
+        if log
+            println(": refined $count triangles")
+        end
+
         refine!(g)
-        match_to_fun!(g, fun)
     end
 end
